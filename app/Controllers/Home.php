ca4b37fc6118920090a8e2cdf6ed66d5e0f27271
Julpinsyah <julpin.nst@gmail.com>
2020-07-07 11:18:25 +0700

    Membuat projec baru BUKU TAMU ONLINE

---
+++ app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		return view('home/index');
+		//echo "Hallo World, thanks.. di coba aja dulu";
+	}
+
+	//--------------------------------------------------------------------
+
+}
