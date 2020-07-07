1737b936e0831b71e11e916f6c07bde4ab08d10d
Julpinsyah <julpin.nst@gmail.com>
2020-07-07 11:22:53 +0700

    merubah title halaman utama

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,22 +1,25 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= base_url('res/bootstrap4/css/bootstrap.css') ?>">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= base_url('res/bootstrap4/css/bootstrap.css') ?>">
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="<?= base_url('res/bootstrap4/js/jquery-3.5.0.js') ?>"></script>
-    <script src="<?= base_url('res/bootstrap4/js/popper.js') ?>"></script>
-    <script src="<?= base_url('res/bootstrap4/js/bootstrap.js') ?>"></script>
-  </body>
+  <title>Buku Tamu Online</title>
+</head>
+
+<body>
+  <h1>Program buku tamu online ku</h1>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="<?= base_url('res/bootstrap4/js/jquery-3.5.0.js') ?>"></script>
+  <script src="<?= base_url('res/bootstrap4/js/popper.js') ?>"></script>
+  <script src="<?= base_url('res/bootstrap4/js/bootstrap.js') ?>"></script>
+</body>
+
 </html>
